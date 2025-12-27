7095bd86d55e85bc27df8db2ba0484e3f414b391
nadjalucic <lucic.sv50.2023@uns.ac.rs>
2025-12-27 11:59:04 +0100

    Add models for vehicle, user, ride, driver

--- backend/src/main/java/Nuvola/Projekatsiit2025/model/Ride.java
+++ backend/src/main/java/Nuvola/Projekatsiit2025/model/Ride.java
@@ -1,4 +1,104 @@
 package Nuvola.Projekatsiit2025.model;
 
 public class Ride {
+
+    private Long id;
+    private double price;
+    private RideStatus status;
+    private String startLocation;
+    private String destination;
+    private String startTime;
+    private String endTime;
+    private String creationDate;
+    private boolean isPanic;
+
+    public Ride() {
+        this.isPanic = false; // default vrednost
+    }
+
+    public Ride(Long id, double price, RideStatus status, String startLocation,
+                String destination, String startTime, String endTime,
+                String creationDate, boolean isPanic) {
+        this.id = id;
+        this.price = price;
+        this.status = status;
+        this.startLocation = startLocation;
+        this.destination = destination;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.creationDate = creationDate;
+        this.isPanic = isPanic;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public RideStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(RideStatus status) {
+        this.status = status;
+    }
+
+    public String getStartLocation() {
+        return startLocation;
+    }
+
+    public void setStartLocation(String startLocation) {
+        this.startLocation = startLocation;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(String creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public boolean isPanic() {
+        return isPanic;
+    }
+
+    public void setPanic(boolean panic) {
+        isPanic = panic;
+    }
 }
--- backend/src/main/java/Nuvola/Projekatsiit2025/model/User.java
+++ backend/src/main/java/Nuvola/Projekatsiit2025/model/User.java
@@ -1,4 +1,92 @@
 package Nuvola.Projekatsiit2025.model;
 
 public class User {
+    protected Long id;
+    protected String email;
+    protected String password;
+    protected String firstName;
+    protected String lastName;
+    protected String address;
+    protected String phone;
+    protected String picture;
+
+    public User() {
+    }
+
+    public User(Long id, String email, String password, String firstName,
+                String lastName, String address, String phone, String picture) {
+        super();
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.phone = phone;
+        this.picture = picture;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
 }
