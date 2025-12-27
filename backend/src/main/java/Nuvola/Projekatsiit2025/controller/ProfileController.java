cb1dd546eb150bce18aa846c7f25a825f78a850a
nadjalucic <lucic.sv50.2023@uns.ac.rs>
2025-12-27 12:01:15 +0100

    Add controller for profile

--- backend/src/main/java/Nuvola/Projekatsiit2025/controller/ProfileController.java
+++ backend/src/main/java/Nuvola/Projekatsiit2025/controller/ProfileController.java
@@ -1,4 +1,39 @@
 package Nuvola.Projekatsiit2025.controller;
 
+
+import Nuvola.Projekatsiit2025.dto.*;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/profile")
 public class ProfileController {
+
+    //2.3
+    @GetMapping
+    public ResponseEntity<ProfileResponseDTO> getProfile() {
+
+        ProfileResponseDTO response = new ProfileResponseDTO();
+        response.setId(1L);
+        response.setEmail("user@mail.com");
+        response.setFirstName("Petar");
+        response.setLastName("Petrovic");
+        response.setPhone("061234567");
+        response.setAddress("Novi Sad");
+
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping
+    public ResponseEntity<Void> updateProfile(@RequestBody UpdateProfileDTO dto) {
+
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/password")
+    public ResponseEntity<Void> changePassword(@RequestBody ChangePasswordDTO dto) {
+
+        return ResponseEntity.ok().build();
+    }
 }
