88e6802d77c1abaeddae2b8fee9b9914dfc6fdc0
nadjalucic <lucic.sv50.2023@uns.ac.rs>
2026-01-21 14:52:14 +0100

    Add components for loged in homepage

---
+++ frontend/src/app/logedin.homepage/logedin.page.component/logedin.page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logedin.page.component',
+  imports: [],
+  templateUrl: './logedin.page.component.html',
+  styleUrl: './logedin.page.component.css',
+})
+export class LogedinPageComponent {
+
+}
