554bc7f53122bb0e8a80c07b0f1644e1adcacc55
nadjalucic <lucic.sv50.2023@uns.ac.rs>
2026-01-26 17:49:34 +0100

    Fix error

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,10 +2,6 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
-<<<<<<< HEAD
-
-=======
->>>>>>> cb5b6996e7619917752923edc226152f9e6dcc44
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
