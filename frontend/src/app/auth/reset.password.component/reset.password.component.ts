90732d1430ee070d7936932cf6bb6fa0389a5c0c
nadjalucic <lucic.sv50.2023@uns.ac.rs>
2026-01-28 11:19:08 +0100

    Add changes to reset password component

--- frontend/src/app/auth/reset.password.component/reset.password.component.ts
+++ frontend/src/app/auth/reset.password.component/reset.password.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AuthService } from '../../services/auth.service';
+import { AuthService } from '../../layout/service/auth.service';
 import {
   AbstractControl,
   FormControl,
