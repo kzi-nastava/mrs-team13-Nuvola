2452ea50619d27fb758ceb3f3849f454fa1cd08c
nadjalucic <lucic.sv50.2023@uns.ac.rs>
2026-01-28 11:23:23 +0100

    Improve existing and add new DTOs

--- backend/src/main/java/Nuvola/Projekatsiit2025/dto/DriverProfileResponseDTO.java
+++ backend/src/main/java/Nuvola/Projekatsiit2025/dto/DriverProfileResponseDTO.java
@@ -1,4 +1,120 @@
 package Nuvola.Projekatsiit2025.dto;
 
+import Nuvola.Projekatsiit2025.model.enums.VehicleType;
+
 public class DriverProfileResponseDTO {
+
+    // user
+    private Long id;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private String address;
+    private String picture;
+
+    // vehicle
+    private String model;
+    private VehicleType type;
+    private int numOfSeats;
+    private boolean babyFriendly;
+    private boolean petFriendly;
+
+    public DriverProfileResponseDTO() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public VehicleType getType() {
+        return type;
+    }
+
+    public void setType(VehicleType type) {
+        this.type = type;
+    }
+
+    public int getNumOfSeats() {
+        return numOfSeats;
+    }
+
+    public void setNumOfSeats(int numOfSeats) {
+        this.numOfSeats = numOfSeats;
+    }
+
+    public boolean isBabyFriendly() {
+        return babyFriendly;
+    }
+
+    public void setBabyFriendly(boolean babyFriendly) {
+        this.babyFriendly = babyFriendly;
+    }
+
+    public boolean isPetFriendly() {
+        return petFriendly;
+    }
+
+    public void setPetFriendly(boolean petFriendly) {
+        this.petFriendly = petFriendly;
+    }
 }
--- backend/src/main/java/Nuvola/Projekatsiit2025/dto/UpdateProfileDTO.java
+++ backend/src/main/java/Nuvola/Projekatsiit2025/dto/UpdateProfileDTO.java
@@ -1,18 +1,46 @@
 package Nuvola.Projekatsiit2025.dto;
 
-public class UpdateProfileDTO {
-    private String firstName;
-    private String lastName;
-    private String phone;
-    private String address;
-    private String picture;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Email;
 
-//    private String vehicleModel;
-//    private VehicleType vehicleType;
-//    private String regNumber;
-//    private int numOfSeats;
-//    private boolean babyFriendly;
-//    private boolean petFriendly;
+public class UpdateProfileDTO {
+
+    @NotNull(message = "First name cannot be null")
+    @NotBlank(message = "First name cannot be blank")
+    @Pattern(
+            regexp = "^[A-ZČĆŠĐŽ][a-zčćšđž]+$",
+            message = "First name must start with a capital letter and contain only letters"
+    )
+    @Size(min = 3, max = 255)
+    private String firstName;
+
+    @NotNull(message = "Last name cannot be null")
+    @NotBlank(message = "Last name cannot be blank")
+    @Pattern(
+            regexp = "^[A-ZČĆŠĐŽ][a-zčćšđž]+$",
+            message = "Last name must start with a capital letter and contain only letters"
+    )
+    @Size(min = 3, max = 255)
+    private String lastName;
+
+    @NotNull(message = "Phone number cannot be null")
+    @NotBlank(message = "Phone number cannot be blank")
+    @Pattern(regexp = "^[0-9+\\s]+$",  message = "Phone must be of correct pattern")
+    private String phone;
+
+    @NotNull(message = "Address cannot be null")
+    @NotBlank(message = "Address cannot be blank")
+    @Pattern(
+            regexp = "^[A-ZČĆŠĐŽ][A-Za-zČĆŠĐŽčćšđž0-9\\s]+$",
+            message = "Address must start with a capital letter and contain only letters, numbers and spaces"
+    )
+    @Size(min = 3, max = 255)
+    private String address;
+
+    private String picture;
 
 
     public UpdateProfileDTO() {}
