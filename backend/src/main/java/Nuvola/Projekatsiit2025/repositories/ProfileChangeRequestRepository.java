d59a06eab88a918fa3fbefc72598e0ecae542b36
nadjalucic <lucic.sv50.2023@uns.ac.rs>
2026-01-28 11:21:05 +0100

    Add repository for profile change request

--- backend/src/main/java/Nuvola/Projekatsiit2025/repositories/ProfileChangeRequestRepository.java
+++ backend/src/main/java/Nuvola/Projekatsiit2025/repositories/ProfileChangeRequestRepository.java
@@ -1,4 +1,10 @@
 package Nuvola.Projekatsiit2025.repositories;
 
-public interface ProfileChangeRequestRepository {
+import Nuvola.Projekatsiit2025.model.ProfileChangeRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProfileChangeRequestRepository
+        extends JpaRepository<ProfileChangeRequest, Long> {
 }
