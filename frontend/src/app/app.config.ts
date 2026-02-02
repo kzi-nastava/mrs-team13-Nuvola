29c6ca34db34d07c415ae108cf4731d4e26b1bd0
Vuk Đorđević <vukdj12@gmail.com>
2026-02-02 16:05:47 +0100

    Added interceptor, auth guard and new auth service

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,15 +1,22 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideHttpClient } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { Interceptor } from './auth/services/interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(),
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay()),
+    provideRouter(routes), 
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: Interceptor,
+      multi: true
+    },
+    provideClientHydration(withEventReplay()),
     provideAnimations()
   ]
 };
---
+++ frontend/src/app/auth/services/auth.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {BehaviorSubject, Observable} from "rxjs";
+import {environment} from "../../env/enviroment";
+import {JwtHelperService} from '@auth0/angular-jwt';
+import { AuthResponse } from '../model/auth.response';
+import { Login } from '../model/login.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private headers = new HttpHeaders({
+    'Content-Type': 'application/json',
+    skip: 'true',
+  });
+
+  user$ = new BehaviorSubject("");
+  userState = this.user$.asObservable();
+
+  constructor(private http: HttpClient) {
+    this.user$.next(this.getRole());
+  }
+
+  login(auth: Login): Observable<AuthResponse> {
+    return this.http.post<AuthResponse>(environment.apiHost + '/logIn', auth, {
+      headers: this.headers,
+    });
+  }
+
+  getRole(): any {
+    if (this.isLoggedIn()) {
+      const accessToken: any = localStorage.getItem('user');
+      const helper = new JwtHelperService();
+      return helper.decodeToken(accessToken).role[0].authority;
+    }
+    return null;
+  }
+
+  isLoggedIn(): boolean {
+    return localStorage.getItem('user') != null;
+  }
+
+  setUser(): void {
+    this.user$.next(this.getRole());
+  }
+}
