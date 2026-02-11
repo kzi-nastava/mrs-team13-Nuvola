8414ccea8c2b0897c175367265a0e9659dd0ea41
Vuk Đorđević <vukdj12@gmail.com>
2026-02-11 15:11:02 +0100

    started toast component

---
+++ frontend/src/app/notifications/toast.component/toast.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-toast.component',
+  imports: [],
+  templateUrl: './toast.component.html',
+  styleUrl: './toast.component.css',
+})
+export class ToastComponent {
+
+}
